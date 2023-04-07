67ec95985bc08de94d01066de0c254aa934af684
kavita mali <bsckavita@gmail.com>
2023-04-07 21:38:20 +0530

    kavita

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const { userRouter } = require("./routes/userRoute");
 require("dotenv").config();
 const app = express();
 
+
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Home Page");
